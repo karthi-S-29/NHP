76bd46e364f474c8082efd96145d916931345cca
karthi-S-29 <sskarthi2962002@gmail.com>
2025-01-09 21:22:47 +0530

    new
    new

---
+++ index.js
@@ -0,0 +1,8 @@
+function showsidebar(){
+    const nav_head2=document.querySelector('.nav_head2')
+    nav_head2.style.display='flex';
+}
+function hidesidebar(){
+    const nav_head2=document.querySelector('.nav_head2')
+    nav_head2 .style.display='none';
+}
