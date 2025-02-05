6f59ce61f4441e7cbd584208a1ff0c763934fa38
karthi-S-29 <sskarthi2962002@gmail.com>
2025-02-05 19:12:33 +0530

    side
    icon placed

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 function showsidebar(){
-    const nav_head2=document.querySelector('.nav_head2')
+    let nav_head2=document.querySelector('.nav_head2')
     nav_head2.style.display='flex';
+    
 }
 function hidesidebar(){
-    const nav_head2=document.querySelector('.nav_head2')
+    let nav_head2=document.querySelector('.nav_head2')
     nav_head2 .style.display='none';
+}
+
+function showsidebar(){
+    let xmark1=document.querySelector('.xmark1')
+    xmark1.style.display='flex';
+    let xmark2=document.querySelector('.xmark2')
+    xmark2.style.display='none';
+    
+    
+    
+}
+function hidesidebar(){
+    let xmark1=document.querySelector('.xmark1')
+    xmark1 .style.display='none';
+    let xmark2=document.querySelector('.xmark2')
+    xmark2.style.display='block';
 }
